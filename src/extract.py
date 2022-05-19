2509eb9dc517cf20ebf0df91bbd113232f1c1e37
Tales de Assis Pedroso <dev.talespedroso@gmail.com>
2022-05-19 15:45:41 -0300

    add stub for new function in extract.py to get the geojson from GitHub

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-def extract(url):
+def extract_business_data(url):
     df = pd.read_csv(url, index_col = False, dtype = 'object')
     return df
+
+def extract_geojson(url):
+    pass
